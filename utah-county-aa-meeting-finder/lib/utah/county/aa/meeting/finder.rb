27c23882dfb75be1b2d18ef96c05215226a22040
James Lotruglio <jimlotruglio@yahoo.com>
2018-05-14 19:19:03 +0000

    added gem template

---
+++ utah-county-aa-meeting-finder/lib/utah/county/aa/meeting/finder.rb
@@ -0,0 +1,13 @@
+require "utah/county/aa/meeting/finder/version"
+
+module Utah
+  module County
+    module Aa
+      module Meeting
+        module Finder
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ utah-county-aa-meeting-finder/lib/utah/county/aa/meeting/finder/version.rb
@@ -0,0 +1,11 @@
+module Utah
+  module County
+    module Aa
+      module Meeting
+        module Finder
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
