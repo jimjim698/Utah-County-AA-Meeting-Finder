7b15683ba94aefc9ea49f1af1eef88574f1bea71
James Lotruglio <jimlotruglio@yahoo.com>
2018-05-17 22:07:31 +0000

    broke call method into 3 separate methods

--- utah_county_aa_meeting_finder/lib/utah_county_aa_meeting_finder/cli.rb
+++ utah_county_aa_meeting_finder/lib/utah_county_aa_meeting_finder/cli.rb
@@ -2,16 +2,26 @@ class UtahCountyAaMeetingFinder::CLI
 
 def call
   puts "Welcome to Utah County AA Meeting Finder"
+list_meetings
+end
+
+
+
+def list_meetings
   puts "Please pick a day you would like to view meetings for"
   input = gets.strip
   if input.downcase == "sunday"
     puts "1. Dopeless Hopefiends"
     puts "2. New beginnings"
     puts "3. Time to wake up"
-    binding.pry
+      meeting_address
   end
+end
 
+def meeting_address
 puts "Please select the number of the meeting you would like the address to."
+input = nil
+while input != "exit"
   input = gets.strip
 
   if input.to_i == 1
@@ -19,8 +29,11 @@ puts "Please select the number of the meeting you would like the address to."
 
 elsif input.to_i == 2
     puts "fellowship hall"
+  else
+    puts "Im not sure what that means"
+    list_meetings
   end
-
+end
 end
 
 end
