9d74abb9ab5c66ae2755504a1d4bcf6200fa4765
James Lotruglio <jimlotruglio@yahoo.com>
2018-06-07 04:58:33 +0000

    start to eliminate chosen_meetings array

--- utah_county_aa_meeting_finder/lib/utah_county_aa_meeting_finder/cli.rb
+++ utah_county_aa_meeting_finder/lib/utah_county_aa_meeting_finder/cli.rb
@@ -11,39 +11,49 @@ end
 def list_meetings
   puts "Please pick a day you would like to view meetings for"
   input = gets.strip
-  if input.downcase == "monday"
-    UtahCountyAaMeetingFinder::Meetings.monday
-      meeting_address
-
-    elsif input.downcase == "tuesday"
-      UtahCountyAaMeetingFinder::Meetings.tuesday
-        meeting_address
-      elsif input.downcase == "wednesday"
-        UtahCountyAaMeetingFinder::Meetings.wednesday
-          meeting_address
-        elsif input.downcase == "thursday"
-          UtahCountyAaMeetingFinder::Meetings.thursday
-            meeting_address
-          elsif input.downcase == "friday"
-            UtahCountyAaMeetingFinder::Meetings.friday
-              meeting_address
-            elsif input.downcase == "saturday"
-              UtahCountyAaMeetingFinder::Meetings.saturday
-                meeting_address
-              elsif input.downcase == "sunday"
-                UtahCountyAaMeetingFinder::Meetings.sunday
-                  meeting_address
-                elsif
-                  input.downcase == "exit"
-                  goodbye
-                  else
-                    puts "I'm sorry, I don't understand"
-                    list_meetings
+  # if input.downcase == "monday"
+  #   binding.pry
+  #   UtahCountyAaMeetingFinder::Meetings.monday
+  #     meeting_address
+  #
+  #   elsif input.downcase == "tuesday"
+  #     UtahCountyAaMeetingFinder::Meetings.tuesday
+  #       meeting_address
+  #     elsif input.downcase == "wednesday"
+  #       UtahCountyAaMeetingFinder::Meetings.wednesday
+  #         meeting_address
+  #       elsif input.downcase == "thursday"
+  #         UtahCountyAaMeetingFinder::Meetings.thursday
+  #           meeting_address
+  #         elsif input.downcase == "friday"
+  #           UtahCountyAaMeetingFinder::Meetings.friday
+  #             meeting_address
+  #           elsif input.downcase == "saturday"
+  #             UtahCountyAaMeetingFinder::Meetings.saturday
+  #               meeting_address
+  #             elsif input.downcase == "sunday"
+  #               UtahCountyAaMeetingFinder::Meetings.sunday
+  #                 meeting_address
+  #               elsif
+  #                 input.downcase == "exit"
+  #                 goodbye
+  #               else
+  if ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"].include? input.downcase
+    # do this now
+    # UtahCountyAaMeetingFinder::Meetings.find_by_day(input)
+    meeting_address(input)
+  else
+    puts "I'm sorry, I don't understand"
+    list_meetings
+  end
 end
+
+def display_meetings(day)
+  # iterate through Meeting.find_by_day(day)
+  # print the info you want
 end
 
-
-def meeting_address
+def meeting_address(day)
 puts "Please select the number of the meeting you would like directions for."
 meetings = UtahCountyAaMeetingFinder::Meetings.chosen_address
   input = gets.strip
